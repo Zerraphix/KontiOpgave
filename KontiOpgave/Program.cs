e4ef0221a528eb2aa920c48e9272975661006f0d
Benjamin B. B <Tec@SKAB1-PC-06>
2021-08-23 09:45:02 +0200

    made a comment, check if it works joey

--- KontiOpgave/Program.cs
+++ KontiOpgave/Program.cs
@@ -30,6 +30,8 @@ namespace KontiOpgave
                 passlog = Console.ReadLine();
                 LogInd.Logind(brugerlog, passlog);
                 return;
+
+                //yOooo, testing that commit push laddie
             }
         }
     }
