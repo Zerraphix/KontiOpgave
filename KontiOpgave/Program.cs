ceef8b16dbfd28504031bb87351c4a900539eff3
Joey Knutsson <joey.knutsson@rocketmail.com>
2021-08-23 10:18:16 +0200

    Settings added

--- KontiOpgave/Program.cs
+++ KontiOpgave/Program.cs
@@ -9,20 +9,7 @@ namespace KontiOpgave
             Password();
             static void Password()
             {
-                string password = "Password", bruger = "bruger", samletlog, brugerlog, passlog;
-                Console.Write("Indtast et brugernavn: ");
-                bruger = Console.ReadLine();
-                LogInd.Bruger(bruger);
-                Console.Write("Indtast en kode der vil virke: ");
-                password = Console.ReadLine();
-                LogInd.Password(password);
-                Console.ReadKey();
-
-                samletlog = bruger + " " + password;
-                using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Tec\Desktop\Logind.txt", true))
-                {
-                    file.WriteLine(samletlog);
-                }
+                string password = "Password", bruger = "bruger", samletlog, brugerlog, passlog;               
 
                 Console.WriteLine("Lad os nu prøve at logge ind med de nylige oprettet bruger navn og password.");
                 Console.Write("Brugernavn: ");
---
+++ KontiOpgave/Settings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KontiOpgave
+{
+    class Settings
+    {
+        public void PasswordChanger()
+        {
+            string password = "Password", bruger = "bruger", samletlog, brugerlog, passlog;
+            Console.Write("Indtast et brugernavn: ");
+            bruger = Console.ReadLine();
+            LogInd.Bruger(bruger);
+            Console.Write("Indtast en kode der vil virke: ");
+            password = Console.ReadLine();
+            LogInd.Password(password);
+            Console.ReadKey();
+
+            samletlog = bruger + " " + password;
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Tec\Desktop\Logind.txt", true))
+            {
+                file.WriteLine(samletlog);
+            }
+        }
+    }
+}
