546d6d5736cbbf6bacbeafc99ed855710d1f4ee9
Joey Knutsson <joey.knutsson@rocketmail.com>
2021-08-23 09:10:47 +0200

    Add project files.

---
+++ KontiOpgave/LogInd.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KontiOpgave
+{
+    public class LogInd
+    {
+        public static void Bruger(string bruger)
+        {
+            int fejl = 0;
+            do
+            {
+                if (fejl == 1)
+                {
+                    Console.Write("Du har skrevet et brugernavn som ikke er fuld lowercase, prøv igen: ");
+                    bruger = Console.ReadLine();
+                }
+                if (bruger.All(char.IsLower))
+                {
+                    fejl = 0;
+                }
+                else
+                {
+                    fejl = 1;
+                }
+            }
+            while (fejl != 0);
+        }
+        public static void Password(string password)
+        {
+            int fejl = 0;
+            do
+            {
+                if (fejl == 1)
+                {
+                    Console.Write("Det valgte password følger ikke vores regler, prøv at skrive et ny password: ");
+                    password = Console.ReadLine();
+                }
+                if (password.Any(char.IsUpper) && password.Any(char.IsLower) && password.Any(char.IsDigit) && password.Contains("!") || password.Contains("#") || password.Contains("%"))
+                {
+                    fejl = 0;
+
+                    if (password.Length >= 12)
+                    {
+                        fejl = 0;
+
+                        if (password.Contains(" "))
+                        {
+                            fejl = 1;
+                        }
+                        else
+                        {
+                            fejl = 0;
+
+                        }
+                    }
+                    else
+                    {
+                        fejl = 1;
+                    }
+                }
+                else
+                {
+                    fejl = 1;
+                }
+            }
+            while (fejl != 0);
+        }
+        public static void Logind(string bruger, string kode)
+        {
+            int fejl = 0;
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\joekn\Desktop\Ny mappe (3)\Logind.txt");
+            do
+            {
+                if (fejl == 1)
+                {
+                    Console.WriteLine("Det ser udtil du har tastet noget forkert, prøv igen");
+                    Console.Write("Brugernavn: ");
+                    bruger = Console.ReadLine();
+                    Console.Write("Password: ");
+                    kode = Console.ReadLine();
+                }
+
+                foreach (string line in lines)
+                {
+                    if (line.Contains(bruger))
+                    {
+                        fejl = 0;
+                        if (line.Contains(kode))
+                        {
+                            fejl = 0;
+                        }
+                        else
+                        {
+                            fejl = 1;
+                        }
+                    }
+                    else
+                    {
+                        fejl = 1;
+                    }
+                }
+            }
+            while (fejl != 0);
+            Console.WriteLine("Du er nu logget ind");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ KontiOpgave/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace KontiOpgave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Password()
+            {
+                string password = "bitch", bruger = "bruger", samletlog, brugerlog, passlog;
+                Console.Write("Indtast et brugernavn: ");
+                bruger = Console.ReadLine();
+                LogInd.Bruger(bruger);
+                Console.Write("Indtast en kode der vil virke: ");
+                password = Console.ReadLine();
+                LogInd.Password(password);
+                Console.ReadKey();
+
+                samletlog = bruger + " " + password;
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\joekn\Desktop\Ny mappe (3)\Logind.txt", true))
+                {
+                    file.WriteLine(samletlog);
+                }
+
+                Console.WriteLine("Lad os nu prøve at logge ind med de nylige oprettet bruger navn og password.");
+                Console.Write("Brugernavn: ");
+                brugerlog = Console.ReadLine();
+                Console.Write("Password: ");
+                passlog = Console.ReadLine();
+                LogInd.Logind(brugerlog, passlog);
+                return;
+            }
+        }
+    }
+}
