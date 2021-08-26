550866d6d47580e8d0a5b4e6c5eea2f356bcab20
molthe <maria.danborg@hotmail.com>
2021-08-26 08:47:14 +0200

    tilføjet nogle kommentarer

--- KontiOpgave/Program.cs
+++ KontiOpgave/Program.cs
@@ -6,23 +6,27 @@ namespace KontiOpgave
     {
         static void Main(string[] args)
         {
-
+            // declaring variables
             string brugerlog, passlog;
             MakeOurData makeOurData= new MakeOurData();
             makeOurData.DataCreator();
 
-            Console.WriteLine("Log ind.");
+            // Log in: get inputdata from user 
+            Console.WriteLine("*** Log ind ***");
             Console.Write("Brugernavn: ");
             brugerlog = Console.ReadLine().ToLower();
             Console.Write("Password: ");
-            passlog = Console.ReadLine();            
-            string test = LogInd.Logind(brugerlog, passlog);
+            passlog = Console.ReadLine();  
+            
+            // check if the login is valid
+            // the method also returns the index i for the users folder, so it can be used later in the code
+            string i = LogInd.Logind(brugerlog, passlog);
             Console.WriteLine("Du er nu logget ind");
             Console.ReadKey();
 
             // Sending over to menu
             Menu menu = new Menu();
-            menu.MenuOversigt(test);
+            menu.MenuOversigt(i);
 
         }
     }
