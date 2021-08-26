1c514234fc088649af8bc74a4eff0851633f2d80
molthe <maria.danborg@hotmail.com>
2021-08-26 08:58:47 +0200

    tilføjet kommentarer

--- KontiOpgave/MakeOurData.cs
+++ KontiOpgave/MakeOurData.cs
@@ -9,14 +9,18 @@ namespace KontiOpgave
 {
     class MakeOurData
     {
+        // this method checks if the necessary folders and files exist. If they do not exist, this method creates them
         public void DataCreator()
         {
+            // declare variables for folder
             string RoamingPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
             string userPath = RoamingPath + @"\kontiuser";
+            // checks if the file exists. 
             if (File.Exists(userPath + @"\Logind.txt"))
             {
 
             }
+            // if the file does not exist, it is created with hardcodet data
             else if (!File.Exists(userPath))
             {
                 Directory.CreateDirectory(userPath);
@@ -24,6 +28,8 @@ namespace KontiOpgave
                 {
                     Userfile.Write("guest\njoey\nmaria\nbenjamin");
                 }
+                // four folders are created, one for each user - guest, joey, maria, benjamin. 
+                // each folder gets a default password and a NemKonto
                 for (int i = 1; i < 5; i++)
                 {
                     string userMultiplePath = userPath + @"\" + i;
@@ -32,19 +38,19 @@ namespace KontiOpgave
                         Directory.CreateDirectory(userMultiplePath);
                         string kontierPath = userMultiplePath + @"\konti";
                         Directory.CreateDirectory(kontierPath);
+                        // default password is set
                         using StreamWriter Passwordfile = new(userMultiplePath + @"\Password.txt", append: false);
                         {
                             Passwordfile.WriteAsync("123456");
                         }
+                        // default account is created
                         using StreamWriter NemKontofile = new(kontierPath + @"\NemKonto.txt", append: false);
                         {
                             NemKontofile.WriteAsync("NemKonto\n0");
                         }
-
                     }
                 }
             }
         }
-
     }
 }
