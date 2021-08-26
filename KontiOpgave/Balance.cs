cf80f375ff98448aa0b39034048a3028d5dcdedf
molthe <maria.danborg@hotmail.com>
2021-08-26 09:07:56 +0200

    tilføjet kommentarer

--- KontiOpgave/Balance.cs
+++ KontiOpgave/Balance.cs
@@ -10,14 +10,20 @@ namespace KontiOpgave
 {
     public class Balance
     {
+        // this method gets the current balance on the user's accounts
         public void BalanceMethod(string i)
         {
+            // declare variables
             string RoamingPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
             string AccountPath;
-            AccountPath = RoamingPath + @"\kontiuser\" + i + @"\konti\" ;
-            // Takes all the files from the folder/path "kontier" and puts them into a string array
+
+            // we use the index i to identify the correct user
+            AccountPath = RoamingPath + @"\kontiuser\" + i + @"\konti\";
+            
+            // Takes all the files from the folder/path "kontier" and puts them into a string array.
             string[] accounts = Directory.GetFiles(AccountPath);
 
+            // goes through the string array and gets the last string in each file, this is the current balance
             foreach (string account in accounts)
             {
                 string[] line = System.IO.File.ReadAllLines(account);
